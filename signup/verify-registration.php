f358f0bb48e4ae26b30be5d8ef595749c6ef2b2e
xmartina <martinsprince126@gmail.com>
2024-04-04 11:42:19 +0100

    d

--- signup/verify-registration.php
+++ signup/verify-registration.php
@@ -1,55 +1,43 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require '../include/vendor/autoload.php'; // Include Composer's autoloader
+require '../include/vendor/autoload.php';
 $pageName  = "Registration";
 require_once './layout/header.php';
 
 if(isset($_POST['regSubmit'])){
-
-    $acct_no = "9909".(substr(number_format(time() * rand(), 0, '', ''), 0, 6));
-    $acct_type = $_POST['acct_type'];
-    $acct_currency = $_POST['acct_currency'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $acct_occupation = $_POST['occupation'];
-    $acct_status = "hold";
-    $country = $_POST['country'];
-    $acct_gender = $_POST['radio-name'];
-    $address = $_POST['address'];
-    $suite = $_POST['suite'];
-    $city = $_POST['city'];
-    $state = $_POST['state'];
-    $zipcode = $_POST['zipcode'];
-    $acct_address = $address." ".$suite. " ".$city." ".$state." ".$zipcode;
-    $acct_email = $_POST['acct_email'];
-    $acct_phone = $_POST['phoneNumber'];
-    $acct_username = $_POST['username'];
-    $acct_password = $_POST['acct_password'];
-    $confirmPassword = $_POST['confirmPassword'];
-    // $ssn = $_POST['ssn'];
-    // $confirm_ssn = $_POST['confirm-ssn'];
-    // $acct_dob = $_POST['dob'];
-    $acct_pin = inputValidation($_POST['acct_pin']);
-    //Defaults
-    $acct_dob = 0;
-    $frontid = 'null';
-    $backId = 'null';
+    // Initialize variables
+    $acct_no = "9909" . substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+    $acct_type = isset($_POST['acct_type']) ? $_POST['acct_type'] : null;
+    $acct_currency = isset($_POST['acct_currency']) ? $_POST['acct_currency'] : null;
+    $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
+    $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
+    $acct_occupation = isset($_POST['occupation']) ? $_POST['occupation'] : null;
+    $acct_status = "hold"; // Default value
+    $country = isset($_POST['country']) ? $_POST['country'] : null;
+    $acct_gender = isset($_POST['radio-name']) ? $_POST['radio-name'] : null;
+    $address = isset($_POST['address']) ? $_POST['address'] : null;
+    $suite = isset($_POST['suite']) ? $_POST['suite'] : null;
+    $city = isset($_POST['city']) ? $_POST['city'] : null;
+    $state = isset($_POST['state']) ? $_POST['state'] : null;
+    $zipcode = isset($_POST['zipcode']) ? $_POST['zipcode'] : null;
+    $acct_address = "$address $suite $city $state $zipcode";
+    $acct_email = isset($_POST['acct_email']) ? $_POST['acct_email'] : null;
+    $acct_phone = isset($_POST['phoneNumber']) ? $_POST['phoneNumber'] : null;
+    $acct_username = isset($_POST['username']) ? $_POST['username'] : null;
+    $acct_password = isset($_POST['acct_password']) ? $_POST['acct_password'] : null;
+    $confirmPassword = isset($_POST['confirmPassword']) ? $_POST['confirmPassword'] : null;
+    $acct_pin = isset($_POST['acct_pin']) ? $_POST['acct_pin'] : null;
+    // Set default values for SSN
     $ssn = 0;
-    $confirm_ssn =0;
-    $n = 'null';
-
-
+    $confirm_ssn = 0;
 
     if($acct_password !== $confirmPassword){
         notify_alert('Password not matched','danger','3000','close');
-
-    }elseif ($ssn !== $confirm_ssn){
+    } elseif ($ssn !== $confirm_ssn){
         notify_alert('SSN / TIN not matched','danger','3000','close');
-
-    }else {
-        //checking exiting email
-
+    } else {
+        // Prepare and execute SQL query (use prepared statements to prevent SQL injection)
         $usersVerified = "SELECT * FROM users WHERE acct_email=:acct_email or acct_username=:acct_username";
         $stmt = $conn->prepare($usersVerified);
         $stmt->execute([
@@ -57,21 +45,18 @@ if(isset($_POST['regSubmit'])){
             'acct_username' => $acct_username
         ]);
 
-
         if ($stmt->rowCount() > 0) {
-            notify_alert('Email or Username Already Exit', 'danger', '3000', 'close');
+            notify_alert('Email or Username Already Exist', 'danger', '3000', 'close');
         } else {
-
-
-            //INSERT INTO DATABASE
-            $registered = "INSERT INTO users (acct_username,firstname,lastname,acct_email,acct_password,acct_no,acct_type,acct_gender,acct_currency,acct_status,acct_phone,acct_occupation,country,state,acct_address,acct_dob,acct_pin,ssn,frontID,backID,image) VALUES(:acct_username,:firstname,:lastname,:acct_email,:acct_password,:acct_no,:acct_type,:acct_gender,:acct_currency,:acct_status,:acct_phone,:acct_occupation,:country,:state,:acct_address,:acct_dob,:acct_pin,:ssn,:frontID,:backID,:image)";
+            // INSERT INTO DATABASE (use prepared statements to prevent SQL injection)
+            $registered = "INSERT INTO users (acct_username, firstname, lastname, acct_email, acct_password, acct_no, acct_type, acct_gender, acct_currency, acct_status, acct_phone, acct_occupation, country, state, acct_address, acct_dob, acct_pin, ssn, frontID, backID, image) VALUES(:acct_username, :firstname, :lastname, :acct_email, :acct_password, :acct_no, :acct_type, :acct_gender, :acct_currency, :acct_status, :acct_phone, :acct_occupation, :country, :state, :acct_address, :acct_dob, :acct_pin, :ssn, :frontID, :backID, :image)";
             $reg = $conn->prepare($registered);
             $reg->execute([
                 'acct_username' => $acct_username,
                 'firstname' => $firstname,
                 'lastname' => $lastname,
                 'acct_email' => $acct_email,
-                'acct_password' => password_hash((string)$acct_password, PASSWORD_BCRYPT),
+                'acct_password' => password_hash($acct_password, PASSWORD_BCRYPT),
                 'acct_no' => $acct_no,
                 'acct_type' => $acct_type,
                 'acct_gender' => $acct_gender,
@@ -82,43 +67,36 @@ if(isset($_POST['regSubmit'])){
                 'country' => $country,
                 'state' => $state,
                 'acct_address' => $acct_address,
-                'acct_dob' => $acct_dob,
+                'acct_dob' => 0, // You may change this to your requirement
                 'acct_pin' => $acct_pin,
-                'ssn' => $ssn,
-                'frontID' => $frontid,
-                'backID' => $backId,
-                'image'=>$n
+                'ssn' => 0, // You may change this to your requirement
+                'frontID' => 'null', // Default values
+                'backID' => 'null', // Default values
+                'image' => 'null' // Default values
             ]);
 
-
-            if (true) {
-
-                // if ($acct_currency === 'USD') {
-                //     $currency = "$";
-                // } elseif ($acct_currency === 'EUR') {
-                //     $currency = "&euro;";
-                // }
-
-                $fullName = $firstname . " " . $lastname;
-                //EMAIL SENDING
-                $email = $acct_email;
+            if ($reg) {
+                // Email Sending logic
+                $fullName = "$firstname $lastname";
                 $APP_NAME = $pageTitle;
                 $APP_URL = WEB_URL;
-                $message = $sendMail->regMsgUser($fullName,$acct_no,$acct_status,$acct_email,$acct_phone,$acct_type,$acct_pin,$APP_NAME,$APP_URL);
-                //User Email
+                $message = $sendMail->regMsgUser($fullName, $acct_no, $acct_status, $acct_email, $acct_phone, $acct_type, $acct_pin, $APP_NAME, $APP_URL);
+
+                // User Email
                 $subject = "Register - $APP_NAME";
-                $email_message->send_mail($email, $message, $subject);
+                $email_message->send_mail($acct_email, $message, $subject);
+
                 // Admin Email
                 $subject = "User Register - $APP_NAME";
                 $email_message->send_mail(WEB_EMAIL, $message, $subject);
-            }
 
-
-            if (true) {
                 toast_alert('success', 'Account Created Successfully, Kindly proceed to login', 'Approved');
             } else {
                 toast_alert('error', 'Sorry something went wrong');
             }
+        }
+    }
+}
 
             /*
             if (isset($_FILES['profile_pic'])) {
@@ -215,11 +193,6 @@ if(isset($_POST['regSubmit'])){
 
             */
 
-        }
-    }
-
-
-}
 //require_once './layout/header.php';
 ?>
 
