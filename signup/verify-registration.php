caf89286cf373ea6383a36d1701a5380dc6af0c8
xmartina <martinsprince126@gmail.com>
2024-04-04 11:44:28 +0100

    d

--- signup/verify-registration.php
+++ signup/verify-registration.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 require '../include/vendor/autoload.php';
 $pageName  = "Registration";
 require_once './layout/header.php';
